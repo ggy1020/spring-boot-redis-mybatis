c183507079a3e1258814a7e5426a7124882e9e36
gaoguangyao <15255626300@163.com>
2018-12-12 19:34:39 +0800

    增加自定义测试切换不同的开发环境

--- src/main/java/com/demo/Demo1Application.java
+++ src/main/java/com/demo/Demo1Application.java
@@ -2,11 +2,20 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication(scanBasePackages = "com.demo")
 public class Demo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
+	}
+
+
+	@RequestMapping("/health")
+	public String health(){
+		return "health";
 	}
 }
