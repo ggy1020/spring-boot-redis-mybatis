bdb7b74ca9ffd306cb96de579b9287354ad85e0e
ggy1020 <1304148597@qq.com>
2021-12-05 11:18:28 +0800

    算法编写

--- src/main/java/com/demo/arithmetic/Code_20211117_18.java
+++ src/main/java/com/demo/arithmetic/Code_20211117_18.java
@@ -1,4 +1,3 @@
-/*
 package com.demo.arithmetic;
 
 public class Code_20211117_18 {
@@ -6,8 +5,8 @@ public class Code_20211117_18 {
 
     }
 
-    */
-/**
+/*
+*
      * 时间复杂度，分析一下选择、冒泡、插入排序的时间复杂度，时间复杂度，只关心高阶项
      * 最难算情况估计时间负载度
      *
@@ -16,7 +15,8 @@ public class Code_20211117_18 {
      * @param arr
      * @param num
      * @return
-     *//*
+
+*/
 
 
 
@@ -33,27 +33,24 @@ public class Code_20211117_18 {
     //arr 局部最小值问题，最需要取一个，任意两个相邻的数不等，无序的数组，相邻的数不相等，如果 【0】《[1] 0就是局部最小   【n-2】》[n-1] n-1就是局部最小
     // [n-3] 》【n-2】《[n-1]   n-2局部最小
 
-    */
 /**
      * 如果 【0】> [1]  同时 【n-2] < [n-1] 则 0  -  n-1必存在一个局部最小
      * 如果 【0】> [1]  同时 【mid-1·] < [mid] 则 0  -  mid必存在一个局部最小
      * @param arr
      * @param num
-     * @return
-     *//*
+     * @return*/
+
 
     public static int mostLRigthNoLessNumIndex (int[] arr, int num){
 
     }
 
-    */
 /**
      * arr 保证有序 二分查找找数相等的
      * @return
-     *//*
+     */
 
     public static boolean findTwo(){
 
     }
 }
-*/
